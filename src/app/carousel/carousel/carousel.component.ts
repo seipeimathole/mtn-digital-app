eed7e48b7969d318a81998b2703a4caa4c11b1b8
Seipei <seipeimathole@gmail.com>
2021-04-06 12:14:49 +0200

    draggable cards

--- src/app/carousel/carousel/carousel.component.ts
+++ src/app/carousel/carousel/carousel.component.ts
@@ -8,91 +8,98 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 export class CarouselComponent implements OnInit {
 
   // carousel object
-    public carousel = [
-      {
-        img:'small-card.png',
-        title: 'Mobile internet',
-        subtitle: false,
-        subtitleText: 'start here',
-        current: false
-      },
-      {
-        img:'small-card.png',
-        title: 'Home internet',
-        subtitle: false,
-        subtitleText: 'start here',
-        current: false,
+  public carousel = [
+    {
+      img:'small-card.png',
+      title: 'Mobile internet',
+      subtitle: false,
+      subtitleText: 'start here',
+      current: false
+    },
+    {
+      img:'small-card.png',
+      title: 'Home internet',
+      subtitle: false,
+      subtitleText: 'start here',
+      current: false,
 
-      },
-      {
-        img:'big-card.png',
-        title: 'Get a device',
-        subtitle: false,
-        subtitleText: 'start here',
-        current: true,
+    },
+    {
+      img:'big-card.png',
+      title: 'Get a device',
+      subtitle: false,
+      subtitleText: 'start here',
+      current: true,
+    },
+    {
+      img:'small-card.png',
+      title: 'Add a phone-line',
+      subtitle: false,
+      subtitleText: 'start here',
+      current: false,
+    },
+    {
+      img:'small-card.png',
+      title: 'Upgrade',
+      subtitle: false,
+      subtitleText: 'start here',
+      current: false,
+    },
+  ];
 
-      },
-      {
-        img:'small-card.png',
-        title: 'Add a phone-line',
-        subtitle: false,
-        subtitleText: 'start here',
-        current: false,
+  // Controls
+  public controls = {
+    currentPosition: 2,
+  }
 
-      },
-      {
-        img:'small-card.png',
-        title: 'Upgrade',
-        subtitle: false,
-        subtitleText: 'start here',
-        current: false,
-      }
+  constructor(
+    private _el: ElementRef,
+    private render: Renderer2
+  ) { }
 
-    ];
+  ngOnInit() {
 
-    // Controls
+  }
 
-    public controls = {
-      totalItems: 5,
-      currentPosition: 2,
+  prevElement() {
+    const cardsContainer = this._el.nativeElement.querySelector('.carousel-card .row')
+    const cards = this._el.nativeElement.querySelectorAll('.carousel-card .row > div')
+    const tmpNode = cards[0];
+    cards[0].className = "col-2 box move-out-from-left";
+    setTimeout(function() {
+      if (cards.length > 5) {
+        tmpNode.classList.add("box--hide"); //add class to hide
+
+        cards[5].className = "col-2 box move-to-position5-from-left";
     }
+        cards[1].className = "col-2 box move-to-position1-from-left";
+        cards[2].className = "col-2 box move-to-position2-from-left";
+        cards[3].className = "col-4 box move-to-position3-from-left";
+        cards[4].className = "col-2 box move-to-position4-from-left";
 
+      cards[0].remove()
+    }, 500);
+    cardsContainer.insertAdjacentHTML('beforeend', tmpNode.outerHTML)
+  }
 
-    constructor(private _el: ElementRef,
-                private render: Renderer2) { }
-
-    ngOnInit() {
-
-    }
-
-    prevElement() {
-      // const elements = this._el.nativeElement.querySelectorAll('.carousel-card .row > div')
-      // this.render.setStylthe(elements[0], 'width', '0px');
-      // is.carousel[2].current = false;
-      // this.carousel[3].current = true;
-      this.moveCarouselArray(this.carousel, 0, 4);
-      this.controls.currentPosition--;
-      this.carousel.forEach(element => {
-        element.current = false;
-      });
-      this.carousel[2].current = true;
-    }
-
-    nextElement() {
-      this.moveCarouselArray(this.carousel, 4, 0);
-      this.controls.currentPosition++;
-      this.carousel.forEach(element => {
-        element.current = false;
-      });
-      this.carousel[2].current = true;
-    }
-
-    moveCarouselArray(arr: any, from: any, to: any) {
-      // Delete the item from it's current position
-      const item = arr.splice(from, 1);
-
-      // Move the item to its new position
-       arr.splice(to, 0, item[0]);
-    }
-
+  nextElement() {
+    const cards = this._el.nativeElement.querySelectorAll('.carousel-card .row > div')
+    const cardsContainer = this._el.nativeElement.querySelector('.carousel-card .row')
+    const noOfCards = cards.length;
+    const tmpNode = cards[noOfCards - 1];
+    cards[4].className = "col-2 box move-out-from-right";
+    setTimeout(function() {
+        if (noOfCards > 4) {
+            cards[4].className = "col-2 box box--hide";
+        }
+        tmpNode.classList.remove("box--hide");
+        cards[noOfCards - 1].remove();
+        cardsContainer.insertBefore(tmpNode, cardsContainer.firstChild);
+        tmpNode.className = "col-2 box move-to-position1-from-right";
+        cards[0].className = "col-2 box move-to-position2-from-right";
+        cards[1].className = "col-4 box move-to-position3-from-right";
+        cards[2].className = "col-2 box move-to-position4-from-right";
+        cards[3].className = "col-2 box move-to-position5-from-right";
+    }, 500);
+  }
 }
